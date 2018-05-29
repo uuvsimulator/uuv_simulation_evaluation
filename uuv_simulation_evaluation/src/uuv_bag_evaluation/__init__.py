0ebf75d84f6804b717e1d94dac7a45ee4dbcf35c
Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>
2018-05-29 17:11:23 +0200

    ADD Read all package files
    Signed-off-by: Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>

--- uuv_simulation_evaluation/src/uuv_bag_evaluation/__init__.py
+++ uuv_simulation_evaluation/src/uuv_bag_evaluation/__init__.py
@@ -13,6 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+import glob
+
+all_list = list()
+for f in glob.glob(os.path.dirname(__file__) + '/*.py'):
+    if os.path.isfile(f) and not os.path.basename(f).startswith('_'):
+        all_list.append(os.path.basename(f)[:-3])
+
+__all__ = all_list  
+
 from .error import ErrorSet, TrajectoryError
 from .recording import Recording
 from .evaluation import Evaluation
