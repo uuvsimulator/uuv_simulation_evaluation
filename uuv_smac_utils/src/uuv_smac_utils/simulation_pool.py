2a02384de5fcf50c3711e45c489dab993da77022
Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>
2018-07-06 16:00:46 +0200

    ADD Simulation pool for multi-task scenarios
    Signed-off-by: Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>

--- uuv_smac_utils/src/uuv_smac_utils/simulation_pool.py
+++ uuv_smac_utils/src/uuv_smac_utils/simulation_pool.py
@@ -21,6 +21,7 @@ import datetime
 from copy import deepcopy
 from time import sleep
 import random
+import shutil
 from .utils import *
 from uuv_simulation_runner import SimulationRunner
 from uuv_bag_evaluation import Evaluation
@@ -34,6 +35,7 @@ N_CRASHES = Value('i', 0)
 PROCESS_LOCK = Lock()
 
 THREAD_POOL = None
+
 
 def add_to_crash_log(data):
     assert isinstance(data, dict)
@@ -51,6 +53,7 @@ def add_to_crash_log(data):
     SIMULATION_LOGGER.info('\tSUCCESS=%d' % N_SUCCESS.value)
     SIMULATION_LOGGER.info('\tCRASHED=%d' % N_CRASHES.value)
 
+
 def add_to_run_log(data):
     assert isinstance(data, dict)
 
@@ -66,6 +69,7 @@ def add_to_run_log(data):
     SIMULATION_LOGGER.info('# simulation runs=%d' % N_SIMULATION_RUNS.value)
     SIMULATION_LOGGER.info('\tSUCCESS=%d' % N_SUCCESS.value)
     SIMULATION_LOGGER.info('\tCRASHED=%d' % N_CRASHES.value)
+
 
 def run_simulation(task):
     random.seed()
@@ -85,7 +89,7 @@ def run_simulation(task):
         runner = SimulationRunner(
             opt_config.params, task, opt_config.results_dir, opt_config.record_all)
         runner.run(opt_config.params)
-        sleep(random.random())
+        sleep(random.random() * 5)
 
         recording_dirname = os.path.dirname(runner.recording_filename)
 
@@ -117,10 +121,17 @@ def run_simulation(task):
         add_to_crash_log(dict(
             status=status,
             timestamp=str(datetime.datetime.now().isoformat()),
+            results_dir=runner.current_sim_results_dir,
             message=str(e),
             task=str(task)))
 
-        output = dict(task=str(task), status=status, cost=partial_cost, sim_time=None, message=str(e))
+        output = dict(
+            task=str(task),
+            status=status,
+            cost=partial_cost,
+            sim_time=None,
+            message=str(e),
+            results_dir=runner.current_sim_results_dir)
 
         if runner is not None:
             if not runner.record_all_results:
@@ -196,10 +207,17 @@ def run_simulation(task):
         add_to_crash_log(dict(
             status=status,
             timestamp=str(datetime.datetime.now().isoformat()),
+            results_dir=runner.current_sim_results_dir,
             message=str(e),
             task=str(task)))
 
-        output = dict(status=status, cost=partial_cost, sim_time=None, message=str(e), task=str(task))
+        output = dict(
+            status=status,
+            cost=partial_cost,
+            sim_time=None,
+            message=str(e),
+            task=str(task),
+            results_dir=runner.current_sim_results_dir)
         PROCESS_LOCK.release()
 
         if runner is not None:
@@ -222,7 +240,8 @@ def run_simulation(task):
     sleep(random.random() * 5)
     return output
 
-def start_simulation_pool(max_num_processes=None, tasks=None, log_filename=None, output_dir=None):
+
+def start_simulation_pool(max_num_processes=None, tasks=None, log_filename=None, output_dir=None, del_failed_tasks=False):
     global THREAD_POOL
     init_logger(log_filename)
 
@@ -241,34 +260,92 @@ def start_simulation_pool(max_num_processes=None, tasks=None, log_filename=None,
     else:
         original_results_path = None
 
-    # try:
-    THREAD_POOL = Pool(processes=num_processes)
+    try:
+        THREAD_POOL = Pool(processes=num_processes)
 
-    task_list = tasks
-    if tasks is None:
-        task_list = opt_config.tasks
-    output = THREAD_POOL.map(run_simulation, task_list)
-    # except Exception, e:
-    #     SIMULATION_LOGGER.info('Error! Killing all processes, message=' + str(e))
-    #     if THREAD_POOL is not None:
-    #         THREAD_POOL.terminate()
-    #         THREAD_POOL.join()
-    #         del THREAD_POOL
-    #     THREAD_POOL = None
-    #     return None, None
-    # else:
-    THREAD_POOL.close()
-    THREAD_POOL.join()
+        task_list = tasks
+        if tasks is None:
+            task_list = opt_config.tasks
+        output = THREAD_POOL.map(run_simulation, task_list)
+    except Exception, e:
+        SIMULATION_LOGGER.error('Error! Killing all processes, message=' + str(e))
+        if THREAD_POOL is not None:
+            THREAD_POOL.terminate()
+            THREAD_POOL.join()
+            del THREAD_POOL
+        THREAD_POOL = None
+        return None, None
+    else:
+        THREAD_POOL.close()
+        THREAD_POOL.join()
 
     if THREAD_POOL is not None:
         del THREAD_POOL
     THREAD_POOL = None
 
     failed_tasks = list()
+    has_crashed = True
+    counter = 0
 
-    for item in output:
-        if item['status'] == SIM_CRASHED:
-            failed_tasks.append(item['task'])
+    while has_crashed and counter < 3:
+        failed_tasks = list()
+        SIMULATION_LOGGER.warning('List of outputs=' + str(output))
+        for i in range(len(output)):
+            if output[i]['status'] == SIM_CRASHED:
+                failed_tasks.append(i)
+
+        if len(failed_tasks) == 0:
+            has_crashed = False
+            break
+
+        SIMULATION_LOGGER.error('Some task runs have crashed, list=' + str(failed_tasks))
+        SIMULATION_LOGGER.error('Rerun counter=' + str(counter))
+
+        for i in failed_tasks:
+            SIMULATION_LOGGER.warning('Renaming folder from failed task:')
+            SIMULATION_LOGGER.warning('\t From: ' + output[i]['results_dir'])
+            SIMULATION_LOGGER.warning('\t To: failed_' + output[i]['results_dir'])
+
+            if os.path.isdir(output[i]['results_dir']):
+                if not del_failed_tasks:
+                    failed_path, failed_dir = os.path.split(output[i]['results_dir'])
+                    os.rename(output[i]['results_dir'], os.path.join(failed_path, 'failed_' + failed_dir))
+                    SIMULATION_LOGGER.warning('Failed task directory renamed=' + os.path.join(failed_path, 'failed_' + failed_dir))
+                else:
+                    shutil.rmtree(output[i]['results_dir'])
+                    SIMULATION_LOGGER.warning('Failed task directory deleted=' + output[i]['results_dir'])
+
+            SIMULATION_LOGGER.info('Running task %d <%s>' % (i, output[i]['task']))
+
+            try:
+                THREAD_POOL = Pool(processes=1)
+
+                task_list = tasks
+                if tasks is None:
+                    task_list = opt_config.tasks
+                output[i] = THREAD_POOL.map(run_simulation, [output[i]['task']])[0]
+            except Exception, e:
+                SIMULATION_LOGGER.error('Error! Killing all processes, message=' + str(e))
+                if THREAD_POOL is not None:
+                    THREAD_POOL.terminate()
+                    THREAD_POOL.join()
+                    del THREAD_POOL
+                THREAD_POOL = None
+                return None, None
+            else:
+                THREAD_POOL.close()
+                THREAD_POOL.join()
+
+            if THREAD_POOL is not None:
+                del THREAD_POOL
+            THREAD_POOL = None
+
+        counter += 1
+
+    failed_tasks = list()
+    for i in range(len(output)):
+        if output[i]['status'] == SIM_CRASHED:
+            failed_tasks.append(i)
 
     if original_results_path is not None:
         opt_config.results_dir = original_results_path
@@ -288,10 +365,12 @@ def stop_simulation_pool():
 
 def killall_ros_processes():
     process_names = [
+        'roscore',
         'rosmaster',
         'roslaunch',
         'rosout',
-        'gzserver']
+        'gzserver',
+        'gzclient']
 
     cmd = 'killall -9 '
     for p in process_names:
